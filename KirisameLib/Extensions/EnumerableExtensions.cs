1c56dde8e413d8b42a401136d30c038631b95b8c
霧雨烨 <Web_13234@outlook.com>
2024-08-29 20:20:19 +0800

    集合求笛卡尔积函数扩展

---
+++ KirisameLib/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace KirisameLib.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<(T1, T2)> CrossJoin<T1, T2>(this IEnumerable<T1> first, IEnumerable<T2> second) =>
+        from x in first
+        from y in second
+        select (x, y);
+}
