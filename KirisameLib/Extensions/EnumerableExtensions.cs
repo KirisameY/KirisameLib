0fe26c725cab7256e5b8ac3589d41d1fb1b2a234
霧雨烨 <Web_13234@outlook.com>
2024-08-30 14:43:50 +0800

    扩展CrossJoin方法

--- KirisameLib/Extensions/EnumerableExtensions.cs
+++ KirisameLib/Extensions/EnumerableExtensions.cs
@@ -3,7 +3,11 @@
 public static class EnumerableExtensions
 {
     public static IEnumerable<(T1, T2)> CrossJoin<T1, T2>(this IEnumerable<T1> first, IEnumerable<T2> second) =>
+        CrossJoin(first, second, (x, y) => (x, y));
+
+    public static IEnumerable<TResult> CrossJoin<T1, T2, TResult>
+        (this IEnumerable<T1> first, IEnumerable<T2> second, Func<T1, T2, TResult> func) =>
         from x in first
         from y in second
-        select (x, y);
+        select func(x, y);
 }
