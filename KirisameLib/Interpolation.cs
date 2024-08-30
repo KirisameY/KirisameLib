968ef7b17d90aec946d4718b5be20dfc1145e7f1
霧雨烨 <Web_13234@outlook.com>
2024-08-30 17:50:17 +0800

    Interpolation&mix, 半途

---
+++ KirisameLib/Interpolation.cs
@@ -0,0 +1,57 @@
+﻿namespace KirisameLib;
+
+public class Interpolation
+{
+    //Initializer
+    private Interpolation(Func<double, double> interpolation, bool easeModeIn)
+    {
+        _interpolation = interpolation;
+        _easeModeIn = easeModeIn;
+    }
+
+    public static Interpolation From(Func<double, double> interpolation, bool easeIn = true) => new Interpolation(interpolation, easeIn);
+
+
+    //Func
+    private readonly Func<double, double> _interpolation;
+
+
+    //Ease mode
+    private readonly bool _easeModeIn;
+    private Func<double, double> Reverse() => t => 1 - _interpolation(1 - t);
+
+    private Func<double, double> ForwardReverse() =>
+        t => t < 0.5
+                 ? _interpolation(2 * t) / 2
+                 : 1 - _interpolation(2 - 2 * t) / 2;
+
+    private Func<double, double> ReverseForward() =>
+        t =>
+            t < 0.5
+                ? (1 - _interpolation(1 - 2 * t)) / 2
+                : (1 + _interpolation(2 * t - 1)) / 2;
+
+    public Func<double, double> EaseIn => _easeModeIn ? _interpolation : Reverse();
+    public Func<double, double> EaseOut => _easeModeIn ? Reverse() : _interpolation;
+
+    public Func<double, double> EaseInOut => _easeModeIn ? ForwardReverse() : ReverseForward();
+
+    public Func<double, double> EaseOutIn => _easeModeIn ? ReverseForward() : ForwardReverse();
+
+
+    //Interpolations
+    public static Func<double, double> Exponential(double exponent) => t => Math.Pow(t, exponent);
+
+    public static Interpolation Linear => From(t => t);
+    public static Interpolation Quadratic => From(t => t * t);
+    public static Interpolation Cubic => From(t => t * t * t);
+    public static Interpolation Quartic => From(t => t * t * t * t);
+    public static Interpolation Quintic => From(t => t * t * t * t * t);
+    public static Interpolation Sin => From(t => Math.Sin(t * Math.PI / 2), false);
+    public static Interpolation Circle => From(t => 1 - Math.Sqrt(1 - t * t));
+    public static Interpolation Back => From(t => t * t * ((1.70158 + 1) * t - 1.70158));
+    public static Interpolation Elastic => From(t => 1 - Math.Cos(t * Math.PI * 2 / 0.3) * Math.Exp(-7 * t), false);
+    public static Interpolation Spring => From(t => throw new NotImplementedException());
+    public static Interpolation Bounce => From(t => throw new NotImplementedException());
+    
+}
--- KirisameLib/MathSpark.cs
+++ KirisameLib/MathSpark.cs
@@ -4,6 +4,7 @@ namespace KirisameLib;
 
 public static class MathSpark
 {
+    //Extension of max,min,clamp
     public static T Max<T>(T a, T b)
         where T : INumber<T> =>
         a > b ? a : b;
@@ -18,4 +19,15 @@ public static class MathSpark
 
     public static int Round(float value) => (int)(value + 0.5f);
     public static int Round(double value) => (int)(value + 0.5d);
+
+
+    //Mix with given interpolation
+    public static double Mix(double a, double b, double t, Func<double, double> interpolation) =>
+        a + (b - a) * interpolation(t);
+
+    public static float Mix(float a, float b, double t, Func<double, double> interpolation) =>
+        a + (b - a) * (float)interpolation(t);
+
+    public static T Mix<T>(T a, T b, double t, Func<double, double> interpolation, Func<T, T, double, T> lerp) =>
+        lerp(a, b, interpolation(t));
 }
