1f171da8bfab831b8529eb6310ff4480138f3f7c
霧雨烨 <Web_13234@outlook.com>
2024-09-17 17:56:44 +0800

    Enumerable的一些扩展

--- KirisameLib/Extensions/EnumerableExtensions.cs
+++ KirisameLib/Extensions/EnumerableExtensions.cs
@@ -2,6 +2,19 @@
 
 public static class EnumerableExtensions
 {
+    //single
+    public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+    {
+        foreach (var element in enumerable)
+        {
+            action(element);
+        }
+    }
+
+    public static IEnumerable<TResult> SelectExist<T, TResult>(this IEnumerable<T> enumerable, Func<T, TResult?> func) =>
+        enumerable.Select(func).OfType<TResult>();
+
+    //dual
     public static IEnumerable<(T1, T2)> CrossJoin<T1, T2>(this IEnumerable<T1> first, IEnumerable<T2> second) =>
         CrossJoin(first, second, (x, y) => (x, y));
 
