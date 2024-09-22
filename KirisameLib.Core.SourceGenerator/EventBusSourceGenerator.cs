d5ac6e153ce46b67c413488690fb3b961bacffed
霧雨烨 <Web_13234@outlook.com>
2024-09-22 22:01:08 +0800

    创建源生成器项目，待实现

---
+++ KirisameLib.Core.SourceGenerator/EventBusSourceGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace KirisameLib.Core.SourceGenerator;
+
+[Generator]
+public class EventBusSourceGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ KirisameLib.Core.SourceGenerator/TypeWithAttributeGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace KirisameLib.Core.SourceGenerator;
+
+public class TypeWithAttributeGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var typeDeclarations = context.SyntaxProvider
+            .CreateSyntaxProvider(
+                predicate: static (node, _) => node is TypeDeclarationSyntax,
+                transform: static (context, _) => (TypeDeclarationSyntax)context.Node
+            )
+            .Where(static typeDeclaration => typeDeclaration.AttributeLists.Count > 0);
+        //todo
+    }
+}
