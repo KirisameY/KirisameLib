9faad3fb02955c8a771786ae4dd6329421b35521
霧雨烨 <Web_13234@outlook.com>
2024-09-28 04:53:34 +0800

    String.Join

---
+++ KirisameLib.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,7 @@
+namespace KirisameLib.Core.Extensions;
+
+public static class StringExtensions
+{
+    public static string Join(this IEnumerable<string> parts, string separator) => string.Join(separator, parts);
+    public static string Join(this IEnumerable<string> parts, char separator) => string.Join(separator, parts);
+}
