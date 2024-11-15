22f3968273d09ce97c245458641788cd18ec3e37
霧雨烨 <Web_13234@outlook.com>
2024-11-15 22:44:09 +0800

    调整现存model class至当前规范

--- KirisameLib.Data/Model/ModelParseErrorInfo.cs
+++ KirisameLib.Data/Model/ModelParseErrorInfo.cs
@@ -1,3 +1,6 @@
 namespace KirisameLib.Data.Model;
 
-public record struct ModelParseErrorInfo(int ErrorCount, string[] Messages);
+public record struct ModelParseErrorInfo(int ErrorCount, string[] Messages)
+{
+    public static ModelParseErrorInfo Empty => new(0, []);
+}
