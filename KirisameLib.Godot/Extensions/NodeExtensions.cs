9f80f3240e148d3f4ec5ea7bada09a28dcc8d800
霧雨烨 <Web_13234@outlook.com>
2024-12-04 23:55:58 +0800

    新增Node扩展

---
+++ KirisameLib.Godot/Extensions/NodeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+using KirisameLib.Core.Extensions;
+
+namespace KirisameLib.Godot.Extensions;
+
+public static class NodeExtensions
+{
+    public static void RemoveChildren(this Node node) => node.GetChildren().ForEach(node.RemoveChild);
+
+    public static void RemoveChildren(this Node node, IEnumerable<Node> children) => children.ForEach(node.RemoveChild);
+
+    public static void RemoveChildren(this Node node, Func<Node, bool> predicate) =>
+        node.GetChildren().Where(predicate).ForEach(node.RemoveChild);
+}
