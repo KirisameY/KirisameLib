25e7c9a71778f63369bccf4c05fb7c86df3e5d48
霧雨烨 <Web_13234@outlook.com>
2025-01-17 17:53:17 +0800

    nuget包发布，在想办法解决这个抽象生成器

--- KirisameLib.Event.SourceGenerator/Global.cs
+++ KirisameLib.Event.SourceGenerator/Global.cs
@@ -3,7 +3,7 @@
 public static class Global
 {
     public const string GeneratorName = "KirisamKirisameLib.Event.SourceGenerator";
-    public const string Version = "0.1.0.0";
+    public const string Version = "0.1.5";
     public const string GeneratedCodeAttribute =
         $"[global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"{GeneratorName}\", \"{Version}\")]";
 }
