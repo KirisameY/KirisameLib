a201766f965bac0cd67dfbefa55687f2639dd283
霧雨烨 <Web_13234@outlook.com>
2025-02-20 22:08:25 +0800

    移除无用的Numeric

--- KirisameLib.Data/Register/IRegister.cs
+++ KirisameLib.Data/Register/IRegister.cs
@@ -2,7 +2,7 @@ namespace KirisameLib.Data.Register;
 
 public interface IRegister<T>
 {
-    T this[string id] => GetItem(id);
+    T this[string id] => GetItem(id); //todo: 等待拓展类型
     T GetItem(string id);
     bool ItemRegistered(string id);
 }
