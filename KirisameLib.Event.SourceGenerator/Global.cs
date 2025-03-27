385683b6062912afea3b9bef0985489723d813e3
霧雨烨 <Web_13234@outlook.com>
2025-03-27 20:53:10 +0800

    铸币了，0.1.3已发布

--- KirisameLib.Event.SourceGenerator/Global.cs
+++ KirisameLib.Event.SourceGenerator/Global.cs
@@ -3,7 +3,7 @@
 public static class Global
 {
     public const string GeneratorName = "KirisamKirisameLib.Event.SourceGenerator";
-    public const string Version = "0.1.2";
+    public const string Version = "0.1.3";
     public const string GeneratedCodeAttribute =
         $"[global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"{GeneratorName}\", \"{Version}\")]";
 }
