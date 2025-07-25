01b7f3de87ee80b65865fc4d35e99548070f763b
霧雨烨 <Web_13234@outlook.com>
2025-07-26 00:50:26 +0800

    Core 0.1.5 - Dynamic combined collection view

--- KirisameLib.Core/Collections/CombinedCollectionView.cs
+++ KirisameLib.Core/Collections/CombinedCollectionView.cs
@@ -1,47 +1,5 @@
-using System.Collections;
 using System.Collections.Immutable;
-
-using JetBrains.Annotations;
 
 namespace KirisameLib.Collections;
 
-public class CombinedCollectionView<T>(params IEnumerable<IReadOnlyCollection<T>> collections) : IReadOnlyCollection<T>, ICollection<T>
-{
-    private readonly ImmutableArray<IReadOnlyCollection<T>> _collections = collections.ToImmutableArray();
-
-    [MustDisposeResource]
-    public IEnumerator<T> GetEnumerator() => _collections.SelectMany(collection => collection).GetEnumerator();
-
-    [MustDisposeResource]
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-    public int Count => _collections.Sum(c => c.Count);
-
-    public bool Contains(T item) => _collections.Any(collection => collection.Contains(item));
-
-
-    //ICollection
-    void ICollection<T>.CopyTo(T[] array, int arrayIndex)
-    {
-        if (arrayIndex <= 0)
-            throw new ArgumentOutOfRangeException(nameof(arrayIndex), arrayIndex, "Param arrayIndex must greater than 0");
-
-        using var enumerator = GetEnumerator();
-        for (int i = arrayIndex; i < array.Length && enumerator.MoveNext(); i++)
-        {
-            array[i] = enumerator.Current;
-        }
-        if (enumerator.MoveNext())
-            throw new ArgumentException("The number of elements in the source collection is greater than"
-                                      + " the available space from arrayIndex to the end of the destination array {array}.");
-    }
-
-    //Readonly
-    bool ICollection<T>.IsReadOnly => true;
-
-    void ICollection<T>.Add(T item) => throw new NotSupportedException();
-
-    void ICollection<T>.Clear() => throw new NotSupportedException();
-
-    bool ICollection<T>.Remove(T item) => throw new NotSupportedException();
-}
+public class CombinedCollectionView<T>(params IEnumerable<IReadOnlyCollection<T>> collections) : DynamicCombinedCollectionView<T>(collections.ToImmutableArray());
--- KirisameLib.Core/Collections/CombinedListView.cs
+++ KirisameLib.Core/Collections/CombinedListView.cs
@@ -1,33 +1,5 @@
-using System.Collections;
 using System.Collections.Immutable;
-
-using JetBrains.Annotations;
 
 namespace KirisameLib.Collections;
 
-public class CombinedListView<T>(params IEnumerable<IReadOnlyList<T>> lists) : IReadOnlyList<T>
-{
-    private ImmutableArray<IReadOnlyList<T>> _lists = lists.ToImmutableArray();
-
-    [MustDisposeResource]
-    public IEnumerator<T> GetEnumerator() => _lists.SelectMany(list => list).GetEnumerator();
-
-    [MustDisposeResource]
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-    public int Count => _lists.Sum(list => list.Count);
-
-    public T this[int index]
-    {
-        get
-        {
-            foreach (var list in _lists)
-            {
-                if (index >= list.Count) index -= list.Count;
-                else return list[index];
-            }
-
-            throw new IndexOutOfRangeException();
-        }
-    }
-}
+public class CombinedListView<T>(params IEnumerable<IReadOnlyList<T>> lists) : DynamicCombinedListView<T>(lists.ToImmutableArray());
