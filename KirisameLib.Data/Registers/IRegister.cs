2305eb3d70e46cf3add076f98e6a2b81befb2556
霧雨烨 <Web_13234@outlook.com>
2025-08-25 19:23:48 +0800

    Data 0.2.6 - Non-Generic IRegister

--- KirisameLib.Data/Registers/IRegister.cs
+++ KirisameLib.Data/Registers/IRegister.cs
@@ -4,12 +4,10 @@ namespace KirisameLib.Data.Registers;
 ///     Readonly register interface.
 /// </summary>
 /// <typeparam name="TKey">Type of item id</typeparam>
-/// <typeparam name="TItem">Type of item to be registered</typeparam>
-
-public interface IRegister<in TKey, out TItem> where TKey : notnull
+public interface IRegister<in TKey> where TKey : notnull
 {
     //todo: 等待拓展类型
-    TItem this[TKey id] { get; }
+    object? this[TKey id] { get; }
 
     /// <summary>
     ///     Get registered item by id.
@@ -18,10 +16,35 @@ public interface IRegister<in TKey, out TItem> where TKey : notnull
     ///     Note that when implement this method, it should not throw exception when given id is not registered.
     ///     Instead, consider a default value or fallback delegate.
     /// </remarks>
-    TItem GetItem(TKey id);
+    object? GetItem(TKey id);
 
     /// <summary>
     ///     Check if given id is registered.
     /// </summary>
     bool ItemRegistered(TKey id);
+}
+
+/// <summary>
+///     Readonly register interface.
+/// </summary>
+/// <typeparam name="TKey">Type of item id</typeparam>
+/// <typeparam name="TItem">Type of item to be registered</typeparam>
+public interface IRegister<in TKey, out TItem> : IRegister<TKey> where TKey : notnull
+{
+    //todo: 等待拓展类型
+    new TItem this[TKey id] { get; }
+
+    /// <summary>
+    ///     Get registered item by id.
+    /// </summary>
+    /// <remarks>
+    ///     Note that when implement this method, it should not throw exception when given id is not registered.
+    ///     Instead, consider a default value or fallback delegate.
+    /// </remarks>
+    new TItem GetItem(TKey id);
+
+
+    // Default impls
+    object? IRegister<TKey>.this[TKey id] => this[id];
+    object? IRegister<TKey>.GetItem(TKey id) => GetItem(id);
 }
