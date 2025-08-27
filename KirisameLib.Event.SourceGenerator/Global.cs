bcdfcdc94b882b709eb8f06379e5772605c6edde
霧雨烨 <Web_13234@outlook.com>
2025-08-27 18:11:06 +0800

    Event 0.3.1

--- KirisameLib.Event.SourceGenerator/Global.cs
+++ KirisameLib.Event.SourceGenerator/Global.cs
@@ -3,7 +3,7 @@
 public static class Global
 {
     public const string GeneratorName = "KirisamKirisameLib.Event.SourceGenerator";
-    public const string Version = "0.3.0";
+    public const string Version = "0.3.1";
     public const string GeneratedCodeAttribute =
         $"[global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"{GeneratorName}\", \"{Version}\")]";
 }
