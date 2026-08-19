0a0ef2f889202674e3e80ed5136f75e4b2d29deb
霧雨烨 <Web_13234@outlook.com>
2026-08-19 12:42:28 +0800

    Math - Fract

---
+++ KirisameY.MathPlus/Extensions/MathFractExtensions.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+
+namespace KirisameY.MathPlus.Extensions;
+
+public static class MathExtensions
+{
+    extension(System.Math)
+    {
+        [PublicAPI]
+        public static decimal Fract(decimal value) => value - Math.Truncate(value);
+        [PublicAPI]
+        public static double Fract(double value) => value - Math.Truncate(value);
+    }
+
+    extension(System.MathF)
+    {
+        [PublicAPI]
+        public static float Fract(float value) => value - MathF.Truncate(value);
+    }
+}
